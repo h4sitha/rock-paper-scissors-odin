71230fcbd67dccc4603a2075fe75e4e6b629db7e
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-02-13 11:23:01 +0530

    Add the pseudo code of the project

---
+++ script.js
@@ -0,0 +1,28 @@
+// User enters their choice: Rock, Paper or Scissors
+    // Use a prompt method for this
+
+// Create a function to get random choice from [Rock, Paper, Scissors]
+// to assign it as the computer's choice
+    // Create an array containing the three choices
+    // Create a new variable and assign it a random number from 1 to 3
+    // Get the computer's choice by using the random number as the index for the array
+
+// Rock beats Scissors
+// Scissors beats Paper
+// Paper beats Rock
+
+// Create a new function to play the game
+    // Get player's and computer's choices and use an if-else statments
+    // to compare them to get their winner
+    // Use logical operators to check the winner of the round
+        // Players choose Rock and Computer choose Scissors 
+    
+    // Create two new variable to track player's wins and losses
+        // Increment the relevant variable after each round
+    // In the end compare these two value to declare the winner
+
+// This is a 5 round game
+// Create another function and keep the track of the rounds played
+    // Create a new variable to store the number of rounds
+    // Call the above function inside this function to play a round
+    // Use a for loop to call the function until the rounds equal to 5
